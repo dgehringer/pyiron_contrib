3d72a61405f9ca418947f7db25ac09cb3ab28f15
Liam Huber <liam.huber@gmail.com>
2020-02-17 16:07:36 +0100

    Refactor method overwriting decorators
    Now it's easy to decorate NotData too.

--- pyiron_contrib/protocol/lazy.py
+++ pyiron_contrib/protocol/lazy.py
@@ -20,7 +20,11 @@ __status__ = "development"
 __date__ = "Feb 10, 2020"
 
 
-class LinalgSpoof:
+class _LinalgSpoof:
+    """
+    A dummy class for lazy variables to access the `numpy.linalg.norm` method.
+    """
+
     @staticmethod
     def norm(x, ord=2, axis=None, keepdims=False):
         if isinstance(x, NotData):
@@ -29,51 +33,77 @@ class LinalgSpoof:
             return nplanorm(x, ord=ord, axis=axis, keepdims=keepdims)
 
 
-def _set_lazy_magic():
+def _lazy_magic(magic_name, cls, expiring=False):
     """
-    A decorator to give the `Lazy` class all manner of lazy magic methods. These methods return a new instance of the
-    class with the calling instance set as the value and the magic method as the call. The call might be expiring if it
-    is one of the 'augmenting' magic methods that begin with 'i'. This is to stop the call from being re-applied every
-    time we resolve the lazy variable, but rather only the first time.
+    A replacement to magic methods for lazy variables by way of returning another lazy variable.
 
-    Note: Inversion is *not* overwritten, since this is used as syntactic sugar for resolving the lazy variable.
-
-    # TODO: More careful parsing so += will work on immutables.
+    Args:
+        magic_name (str): The name of the python magic method being replaced.
+        cls (Lazy): The class to decorate. It had better be `Lazy`.
+        expiring (bool): Whether the recursing call should delete itself after being used. This should be true when
+            the call is to an in-place modifying method, i.e. those which begin with 'i'.
 
     Returns:
-        (fnc): The decorating function.
+        (fnc): A function returning a new instance of `cls` using the calling instance as its `value`, the new
+            `magic_name` named method as its function call, set to expire based on `expiring`.
     """
-    base_names = [
-        '__getitem__',
-        '__pos__', '__neg__', '__abs__', '__round__', '__floor__', '__ceil__', '__trunc__',
-        '__eq__', '__ne__', '__lt__', '__gt__', '__le__', '__ge__',
-        '__add__', '__sub__', '__mul__', '__floordiv__', '__truediv__', '__mod__', '__divmod__', '__pow__',
-        '__lshift__', '__rshift__', '__and__', '__or__', '__xor__',
-        '__radd__', '__rsub__', '__rmul__', '__rfloordiv__', '__rtruediv__', '__rmod__', '__rdivmod__', '__rpow__',
-        '__rlshift__', '__rrshift__', '__rand__', '__ror__', '__rxor__',
-        '__call__'
-    ]
-    augmenting_names = [
-        '__iadd__', '__isub__', '__imul__', '__ifloordiv__', '__itruediv__', '__imod__', '__idivmod__', '__ipow__',
-        '__ilshift__', '__irshift__', '__iand__', '__ior__', '__ixor__',
-    ]
+    def fn(self, *args, **kwargs):
+        return cls(value=self, call=(magic_name, args, kwargs), expiring_call=expiring)
+    return fn
 
-    def lazy_magic(magic_name, cls, expiring=False):
-        """
 
-        Args:
-            magic_name (str):
-            cls (Lazy): The class to decorate. It had better be `Lazy`.
-            expiring (bool): Whether the recursing call should delete itself after being used. This should be true when
-                the call is to an in-place modifying method, i.e. those which begin with 'i'.
+def _lazy_base_magic(magic_name, cls):
+    return _lazy_magic(magic_name, cls, expiring=False)
 
-        Returns:
-            (cls): A new instance of the class with the calling instance as its `value`, the new `magic_name` named
-                method as its function call, set to expire based on `expiring`.
-        """
-        def fn(self, *args, **kwargs):
-            return cls(value=self, call=(magic_name, args, kwargs), expiring_call=expiring)
-        return fn
+
+def _lazy_augmenting_magic(magic_name, cls):
+    return _lazy_magic(magic_name, cls, expiring=True)
+
+
+def _not_data_magic(magic_name, cls):
+    """
+    A replacement for magic methods for the `NotData` class.
+
+    Args:
+        magic_name (str): The name of the python magic method being replaced.
+        cls (Lazy): The class to decorate. It had better be `Lazy`.
+
+    Returns:
+        (fnc): A new function which just returns self.
+    """
+
+    def fn(self, *args, **kwargs):
+        return self
+    return fn
+
+
+_base_names = [
+    '__getitem__',
+    '__pos__', '__neg__', '__abs__', '__round__', '__floor__', '__ceil__', '__trunc__',
+    '__eq__', '__ne__', '__lt__', '__gt__', '__le__', '__ge__',
+    '__add__', '__sub__', '__mul__', '__floordiv__', '__truediv__', '__mod__', '__divmod__', '__pow__',
+    '__lshift__', '__rshift__', '__and__', '__or__', '__xor__',
+    '__radd__', '__rsub__', '__rmul__', '__rfloordiv__', '__rtruediv__', '__rmod__', '__rdivmod__', '__rpow__',
+    '__rlshift__', '__rrshift__', '__rand__', '__ror__', '__rxor__',
+    '__call__'
+]
+_augmenting_names = [
+    '__iadd__', '__isub__', '__imul__', '__ifloordiv__', '__itruediv__', '__imod__', '__idivmod__', '__ipow__',
+    '__ilshift__', '__irshift__', '__iand__', '__ior__', '__ixor__',
+]
+
+
+def _override_methods(replacement, names):
+    """
+    A decorator to override class methods with the provided replacement function.
+
+    Args:
+        replacement (fnc): The function with which to replace the methods in `base_names`.
+        names (list): The names of methods to be replaced by `replacement`.
+
+    Returns:
+        (cls): A new instance of the class with the magic methods overwritten.
+    """
 
     def decorate(cls):
         """
@@ -84,17 +114,15 @@ def _set_lazy_magic():
         Returns:
             (cls): The class with a bunch of new magic methods.
         """
-        for name in base_names:
-            setattr(cls, name, lazy_magic(name, cls))
-
-        for name in augmenting_names:
-            setattr(cls, name, lazy_magic(name, cls, expiring=True))
+        for name in names:
+            setattr(cls, name, replacement(name, cls))
 
         return cls
     return decorate
 
 
-@_set_lazy_magic()
+@_override_methods(_lazy_base_magic, _base_names)
+@_override_methods(_lazy_augmenting_magic, _augmenting_names)
 class Lazy:
     """
     A class which defers evaluation until its `.resolve` method is called, or it is prepended with a `~`.
@@ -209,7 +237,7 @@ class Lazy:
         returns:
             (float/numpy.ndarray): Norm of the matrix or vector(s).
         """
-        return Lazy(value=Lazy(LinalgSpoof), call=('norm', (self,), {'ord': ord, 'axis': axis, 'keepdims': keepdims}))
+        return Lazy(value=Lazy(_LinalgSpoof), call=('norm', (self,), {'ord': ord, 'axis': axis, 'keepdims': keepdims}))
 
 
 class PatientDict(UserDict):
@@ -237,22 +265,16 @@ class PatientDict(UserDict):
         self.__getitem__(item)
 
 
+@_override_methods(_not_data_magic, _base_names)
+@_override_methods(_not_data_magic, _augmenting_names)
 class NotData(object):
     """A datatype to indicate that an input stack really doesn't have data (since `None` might be valid input!)"""
-    def __eq__(self, other):
-        if isinstance(other, NotData):
-            return True
-        else:
-            return False
 
     def __repr__(self):
         return "<NotData>"
 
     def __str__(self):
         return "notdata"
-
-    def __getitem__(self, item):
-        return self
 
     def __getattribute__(self, item):
         try:
@@ -261,7 +283,4 @@ class NotData(object):
             return self
 
     def __getattr__(self, item):
-        return self
-
-    def __call__(self, *args, **kwargs):
         return self
