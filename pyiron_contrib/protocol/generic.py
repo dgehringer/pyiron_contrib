04beea671b4dfc6e0697cb308b0850afd83c4967
Liam Huber <liam.huber@gmail.com>
2020-09-15 23:46:24 +0200

    Fix loading protocols from hdf

--- pyiron_contrib/protocol/generic.py
+++ pyiron_contrib/protocol/generic.py
@@ -454,8 +454,6 @@ class CompoundVertex(Vertex): #, PyironJobTypeRegistry):
             hdf (ProjectHDFio): HDF5 group object.
             group_name (str): HDF5 subgroup name - optional
         """
-        with hdf.open(self.vertex_name) as graph_hdf:
-            self.graph.to_hdf(hdf=graph_hdf, group_name="graph")
         super(CompoundVertex, self).to_hdf(hdf=hdf, group_name=group_name)
         self.graph.to_hdf(hdf=hdf, group_name="graph")
 
@@ -467,8 +465,9 @@ class CompoundVertex(Vertex): #, PyironJobTypeRegistry):
             hdf (ProjectHDFio): HDF5 group object - optional
             group_name (str): HDF5 subgroup name - optional
         """
-        super(CompoundVertex, self).from_hdf(hdf=hdf, group_name=group_name)
-        self.graph.from_hdf(hdf=hdf, group_name="graph")
+        super(CompoundVertex, self).from_hdf(hdf=hdf, group_name=group_name or self.vertex_name)
+        with hdf.open(self.vertex_name) as hdf5_server:
+            self.graph.from_hdf(hdf=hdf5_server, group_name="graph")
         self.define_information_flow()  # Rewire pointers
 
     def visualize(self, execution=True, dataflow=True):
@@ -669,8 +668,7 @@ class Protocol(CompoundVertex, GenericJob, ABC):
         """
         if hdf is None:
             hdf = self.project_hdf5
-        self.graph.to_hdf(hdf=hdf, group_name="graph")
-        super(CompoundVertex, self).to_hdf(hdf=hdf, group_name=group_name)
+        super(Protocol, self).to_hdf(hdf=hdf, group_name=group_name)
 
     def from_hdf(self, hdf=None, group_name=None):
         """
