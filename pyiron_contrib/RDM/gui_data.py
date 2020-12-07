5eac2d9770ed26e079b579df566bb7147b33c3b9
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2020-12-07 22:40:41 +0100

    Add hdf browse functionality to the file browser

--- pyiron_contrib/RDM/gui_data.py
+++ pyiron_contrib/RDM/gui_data.py
@@ -7,6 +7,8 @@ from IPython.core.display import display
 from matplotlib import pylab as plt
 import pandas
 
+from pyiron_base.generic.hdfio import FileHDFio
+
 from pyiron_contrib.RDM.S3ObjectDB import S3ObjectDB
 from pyiron_contrib.image.image import Image
 from pyiron_contrib.RDM.measurement import MeasuredData
@@ -101,7 +103,8 @@ class _FileBrowser(object):
                  fix_s3_path=False,
                  storage_system="local",
                  fix_storage_sys=False,
-                 S3_config_file=None
+                 S3_config_file=None,
+                 hdf_as_dirs=False
                  ):
         """
             Filebrowser to browse the local or a remote (S3-based) file system.
@@ -112,6 +115,7 @@ class _FileBrowser(object):
               storage_system (str): The filesystem to access (fist) either "local" or "S3".
               fix_storage_sys (bool): If True the file system cannot be changed.
               S3_config_file (str): path to a json configuration file with login credentials for the remote file system.
+              hdf_as_dirs (bool): If True hdf files in the local file system are shown and treated as directories
         """
         if Vbox is None:
             self.box = widgets.VBox()
@@ -119,9 +123,15 @@ class _FileBrowser(object):
             self.box = Vbox
         self.fix_s3_path = fix_s3_path
         self.s3path = s3path
+        if localpath is None:
+            localpath = os.getcwd()
+        self.hdf_as_dirs = hdf_as_dirs
+        self._in_hdf = False
+        self._h5_access = None
+        self._h5_path = ""
         self.data_sys = storage_system
         if self.data_sys == "local":
-            self.path = localpath or os.getcwd()
+            self.path = localpath
         else:
             self.path = self.s3path
         self.output = widgets.Output(layout=widgets.Layout(width='50%', height='100%'))
@@ -130,7 +140,7 @@ class _FileBrowser(object):
         self.fix_storage_sys = fix_storage_sys
         self._data_access = S3ObjectDB(config_file=S3_config_file,
                                        group=self.s3path)
-        self.path_storage = [os.getcwd(), self.s3path]
+        self.path_storage = [localpath, self.s3path]
         self._update_files()
         self.pathbox = widgets.HBox(layout=widgets.Layout(width='100%', justify_content='flex-start'))
         self.optionbox = widgets.HBox()
@@ -143,17 +153,21 @@ class _FileBrowser(object):
                  fix_s3_path=None,
                  storage_system=None,
                  fix_storage_sys=None,
+                 hdf_as_dirs=None
                  ):
         """
             Reconfigure and refresh Filebrowser.
             Args:
               s3path (str/None): Path within the remote file system.
-              fix_s3_path (bool): If True the path in the remote file system cannot be changed.
+              fix_s3_path (bool/None): If True the path in the remote file system cannot be changed.
               storage_system (str/None): The filesystem to access (first): either "local" or "S3".
-              fix_storage_sys (bool): If True the file system cannot be changed.
+              fix_storage_sys (bool/None): If True the file system cannot be changed.
+              hdf_as_dirs (bool/None): If True hdf files in the local file system are shown and treated as directories
         """
         if s3path is not None:
             self.s3path = s3path
+        if hdf_as_dirs is not None:
+            self.hdf_as_dirs = hdf_as_dirs
         if fix_s3_path is not None:
             self.fix_s3_path = fix_s3_path
         if storage_system is not None:
@@ -178,13 +192,23 @@ class _FileBrowser(object):
     def _update_files(self):
         self.files = list()
         self.dirs = list()
-        if self.data_sys == "local":
+        self.h5dirs = list()
+        if self.data_sys == "local" and not self._in_hdf:
             if os.path.isdir(self.path):
                 for f in iglob(self.path + '/*'):
                     if os.path.isdir(f):
                         self.dirs.append(os.path.split(f)[1])
                     else:
-                        self.files.append(os.path.split(f)[1])
+                        filename = os.path.split(f)[1]
+                        if self.hdf_as_dirs and os.path.splitext(filename)[1] in ".h5":
+                            self.h5dirs.append(filename)
+                        else:
+                            self.files.append(filename)
+        elif self._in_hdf:
+            self._h5_access.h5_path = self._h5_path
+            list_all_dir = self._h5_access.list_all()
+            self.files = list_all_dir["nodes"]
+            self.h5dirs = list_all_dir["groups"]
         else:
             self._data_access.open(self.path)
             self.files = self._data_access.list_nodes()
@@ -308,7 +332,10 @@ class _FileBrowser(object):
             else:
                 self.path_string_box.__init__(description="(rel) Path", value='')
                 with self.output:
-                    print('No valid path')
+                    if not self.hdf_as_dirs:
+                        print('No valid path')
+                    else:
+                        print('No valid path or path within h5 (not supported)')
                 return
             self._update_files()
             self._update_filebox(self.filebox)
@@ -381,10 +408,12 @@ class _FileBrowser(object):
         else:
             path = self.path_string_box.value
         appendlist = []
-        if self.data_sys == "local":
+        if self.data_sys == "local" and not self._in_hdf:
             for f in iglob(path):
                 if os.path.isfile(f):
                     appendlist.append(f)
+        elif self._in_hdf:
+            pass
         else:
             appendlist = self._data_access.glob(path)
         self._clickedFiles.extend(appendlist)
@@ -399,15 +428,33 @@ class _FileBrowser(object):
 
     def _update_pathbox(self, box):
         path_color = '#DDDDAA'
+        h5_path_color = '#CCCCAA'
         home_color = '#999999'
 
         def on_click(b):
-            self.path = b.path
+            if not b.h5:
+                self.path = b.path
+                self._h5_access = None
+                self._in_hdf = False
+            else:
+                self._h5_path = b.path
             self._update_files()
             self._update_filebox(self.filebox)
             self.path_string_box.__init__(description="(rel) Path", value='')
 
         buttons = []
+        if self._in_hdf:
+            tmppath = self._h5_path
+            tmppath_old = self._h5_path + '/'
+            while tmppath != tmppath_old:
+                tmppath_old = tmppath
+                [tmppath, dir] = os.path.split(tmppath)
+                button = widgets.Button(description=dir + '/', layout=widgets.Layout(width='auto'))
+                button.style.button_color = h5_path_color
+                button.path = tmppath_old
+                button.h5 = True
+                button.on_click(on_click)
+                buttons.append(button)
         tmppath = self.path
         tmppath_old = self.path + '/'
         while tmppath != tmppath_old:
@@ -416,6 +463,7 @@ class _FileBrowser(object):
             button = widgets.Button(description=dir + '/', layout=widgets.Layout(width='auto'))
             button.style.button_color = path_color
             button.path = tmppath_old
+            button.h5 = False
             button.on_click(on_click)
             if self.fix_s3_path and self.data_sys == "S3":
                 button.disabled = True
@@ -428,6 +476,7 @@ class _FileBrowser(object):
             button.path = self.s3path
             if self.fix_s3_path:
                 button.disabled = True
+        button.h5 = False
         button.on_click(on_click)
         buttons.append(button)
         buttons.reverse()
@@ -436,26 +485,44 @@ class _FileBrowser(object):
     def _update_filebox(self, filebox):
         # color definitions
         dir_color = '#9999FF'
+        h5_dir_color = '#9999EE'
         file_chosen_color = '#FFBBBB'
         file_color = '#DDDDDD'
         self.output.clear_output(True)
 
         def on_click(b):
-            self.path = os.path.join(self.path, b.description)
+            if b.h5:
+                self._h5_path = "/"
+                self._in_hdf = True
+                self._h5_access = FileHDFio(file_name=os.path.join(self.path, b.description),
+                                            h5_path=self._h5_path,
+                                            mode="r")
+            else:
+                self.path = os.path.join(self.path, b.description)
+            self._update_files()
+            self._update_filebox(filebox)
+
+        def on_click_h5(b):
+            self._h5_path = os.path.join(self._h5_path, b.description)
             self._update_files()
             self._update_filebox(filebox)
 
         def on_click_file(b):
             f = os.path.join(self.path, b.description)
             self.output.clear_output(True)
-            if self.data_sys == 'local':
+            if self.data_sys == 'local' and not self._in_hdf:
                 DisplayFile(f, self.output)
+            elif self._in_hdf:
+                with self.output:
+                    print(self._h5_access[b.description])
             else:
                 metadata = self._data_access.get_metadata(f, abspath=True)
                 DisplayMetadata(metadata, self.output)
             if f in self._clickedFiles:
                 b.style.button_color = file_color
                 self._clickedFiles.remove(f)
+            elif self._in_hdf:
+                pass
             else:
                 b.style.button_color = file_chosen_color
                 self._clickedFiles.append(f)
@@ -472,8 +539,21 @@ class _FileBrowser(object):
                                     icon="fa-folder",
                                     layout=item_layout)
             button.style.button_color = dir_color
+            button.h5 = False
             button.on_click(on_click)
             buttons.append(button)
+        for f in self.h5dirs:
+            button = widgets.Button(description=f,
+                                    icon="fa-folder",
+                                    layout=item_layout)
+            button.style.button_color = h5_dir_color
+            if not self._in_hdf:
+                button.h5 = True
+                button.on_click(on_click)
+            else:
+                button.on_click(on_click_h5)
+            buttons.append(button)
+
         for f in self.files:
             button = widgets.Button(description=f,
                                     icon="fa-file-o",
@@ -527,9 +607,10 @@ class FileBrowser(_FileBrowser):
                  project=None,
                  Vbox = None,
                  s3path="",
-                 fix_s3_path = False,
-                 storage_system = "local",
-                 fix_storage_sys = False,
+                 fix_s3_path=False,
+                 storage_system="local",
+                 fix_storage_sys=False,
+                 hdf_as_dirs=False,
                  S3_config_file = None
                  ):
 
@@ -538,7 +619,9 @@ class FileBrowser(_FileBrowser):
                          s3path=s3path,
                          localpath=path,
                          fix_s3_path=fix_s3_path,
+                         hdf_as_dirs=hdf_as_dirs,
                          storage_system=storage_system,
                          fix_storage_sys=fix_storage_sys,
                          S3_config_file=S3_config_file)
+    __init__.__doc__ = _FileBrowser.__init__.__doc__
 
