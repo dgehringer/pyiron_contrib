4d50511254ee142752bcf49f3dedd6472d19d6b1
dnoeger <dominik.noeger@unileoben.ac.at>
2021-03-11 16:32:38 +0100

    Removed BFGS algos from __init__.py

--- pyiron_contrib/__init__.py
+++ pyiron_contrib/__init__.py
@@ -1,13 +1,11 @@
 # from pyiron_contrib.protocol.generic import Protocol
 from pyiron_contrib.protocol.compound.minimize import ProtocolMinimize
-from pyiron_contrib.protocol.compound.minimize_bfgs import ProtocolMinimizeBFGS
 from pyiron_contrib.protocol.compound.molecular_dynamics import ProtocolMD
 from pyiron_contrib.protocol.compound.nudged_elastic_band import ProtocolNEB
 # from pyiron_contrib.protocol.compound.tild import HarmonicTILD, VacancyTILD
 # from pyiron_contrib.protocol.compound.finite_temperature_string import StringRelaxation, VirtualWork, \
 #     Milestoning, VirtualWorkParallel, VirtualWorkSerial, VirtualWorkFullStep
 from pyiron_contrib.protocol.compound.qmmm import ProtocolQMMM
-from pyiron_contrib.protocol.compound.qmmm_bfgs import ProtocolQMMMBFGS
 from pyiron_contrib.protocol.compound.tild import ProtocolHarmonicTILD, ProtocolHarmonicTILDParallel
 
 # protocol is a magic class after this one we have to Register
